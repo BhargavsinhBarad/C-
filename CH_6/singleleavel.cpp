5e5fa1286ac644b37b2eda893e3074ae970bc706
Bhargavsinh  M Barad <118417960+BhargavsinhBarad@users.noreply.github.com>
2023-02-17 10:18:29 +0530

    INHERITANCE

---
+++ CH_6/singleleavel.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class x{
+	protected :
+		int a,b,c;
+};
+
+class y : public x{
+	public :
+		void setdata()
+		{
+			cout << "Enter a : ";
+			cin >> a;
+			cout << "Enter b : ";
+			cin >> b;
+			cout << "Enter c : ";
+			cin >> c;
+		}
+		void getdata()
+		{
+			cout << "A : " << a << endl
+				 << "B : " << b << endl
+				 << "C : " << c << endl;
+		}
+};
+
+int main()
+{
+	y z;
+	
+	z.setdata();
+	z.getdata();
+}
